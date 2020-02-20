3131393b6280fcf19a75cfe5cf3e32b322a95407
noscam0012 <61249481+noscam0012@users.noreply.github.com>
2020-02-20 14:09:31 +0700

    AddDialogueFiles

---
+++ DialougeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text dialoguetext;
+    public Animator animator;
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        animator.SetBool("Isopen", true);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialoguetext.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialoguetext.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("Isopen", false);
+
+    }
+
+
+}
